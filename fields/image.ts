c4bc2769ac2a96ddeb17f14d2e1f0fe6abdf639d
Oscar Otero <oom@oscarotero.com>
2022-06-01 12:36:11 +0200

    fixed defaults for image widget

--- fields/image.ts
+++ fields/image.ts
@@ -3,6 +3,12 @@ import File, { Options as FileOptions } from "./file.ts";
 export type Options = FileOptions;
 
 export default class Image extends File {
+  static defaults: Options = {};
   widget = "image";
   config: Options = {};
+
+  constructor(label: string) {
+    super(label);
+    this.config = structuredClone(File.defaults);
+  }
 }
